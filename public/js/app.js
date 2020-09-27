258100c947f5c6506201f320b32b9ef9a40dd9a8
Jeel Baraiya <jeelrb30@gmail.com>
2020-09-27 17:04:36 +0530

    Remove unecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-console.log('Javascript!!');
-
-
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const error = document.querySelector('.msg-1');
@@ -13,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
   const address = search.value;
   error.textContent = ''
   forecast.textContent = ''
-  
+
   fetch('http://localhost:3000/weather?address='+address).then((response) => {
     response.json().then((data) => {
       if(data.error){
